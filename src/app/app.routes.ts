16137fe095a3188deb4f6ccc6b15924c802de853
RCSilva29 <avliscleafar@gmail.com>
2025-06-02 21:59:30 -0300

    Atualizando projeto com a aula do dia 02JUN2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { CadastroTarefasComponent } from './components/pages/cadastro-tarefas/cadastro-tarefas.component';
+import { ConsultaTarefasComponent } from './components/pages/consulta-tarefas/consulta-tarefas.component';
+import { EdicaoTarefasComponent } from './components/pages/edicao-tarefas/edicao-tarefas.component';
 
-export const routes: Routes = [];
+// Mapeamento das rotas de navegacao do projeto
+export const routes: Routes = [
+    {
+        path: 'pages/cadastro-tarefas', // rota de navegacao
+        component: CadastroTarefasComponent, // componente        
+    },
+    {
+        path: 'pages/consulta-tarefas', // rota de navegacao
+        component: ConsultaTarefasComponent // componente
+    },
+    {
+        path: 'pages/edicao-tarefas', // rota de navegacao
+        component: EdicaoTarefasComponent // componente
+    }
+];
--- src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
+++ src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-tarefas',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './cadastro-tarefas.component.html',
   styleUrl: './cadastro-tarefas.component.css'
 })
 export class CadastroTarefasComponent {
 
+  // atributos da classe
+  categorias : any[] = []; // array de objetos vazio
+
+  //injecao de dependencia do HttpClient
+  http = inject(HttpClient);
+
+  //funcao executada quando o componente é inicializado
+  ngOnInit() {
+    //faz uma requisicao HTTP GET para consultar as categorias
+    this.http.get('http://localhost:8081/api/v1/categorias')
+      .subscribe((response) => { // capturando a resposta da API
+        // armazenarndo a resposta obtida da API no atributo da classe
+        this.categorias = response as any[];
+      })
+  }
 }
