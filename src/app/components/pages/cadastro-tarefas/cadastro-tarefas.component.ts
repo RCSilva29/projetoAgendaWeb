ffbadee8768346b21f0615db2d4e8bfab71c1988
RCSilva29 <avliscleafar@gmail.com>
2025-06-09 16:43:14 -0300

    Versionando aula do dia 04JUN2025

--- src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
+++ src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-cadastro-tarefas',
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './cadastro-tarefas.component.html',
   styleUrl: './cadastro-tarefas.component.css'
@@ -13,18 +17,40 @@ import { Component, inject } from '@angular/core';
 export class CadastroTarefasComponent {
 
   // atributos da classe
-  categorias : any[] = []; // array de objetos vazio
+  categorias: any[] = []; // array de objetos vazio
+  mensagem : string = ''; // string de texto vazia
 
-  //injecao de dependencia do HttpClient
+  //injecoes de dependencia
   http = inject(HttpClient);
+  fb = inject(FormBuilder);
+
+  //estrutura do formulario
+  form = this.fb.group({
+    titulo: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(150)]),
+    data: new FormControl('', [Validators.required]),
+    hora: new FormControl('', [Validators.required]),
+    finalizado: new FormControl('', [Validators.required]),
+    categoriaId: new FormControl('', [Validators.required])
+  });
 
   //funcao executada quando o componente é inicializado
   ngOnInit() {
     //faz uma requisicao HTTP GET para consultar as categorias
-    this.http.get('http://localhost:8081/api/v1/categorias')
+    this.http.get(environment.apiTarefas + '/categorias')
       .subscribe((response) => { // capturando a resposta da API
         // armazenarndo a resposta obtida da API no atributo da classe
         this.categorias = response as any[];
       })
+  }
+
+  //funcao executada quando o usuario clica no botao submit do formulario
+  onSubmit() {
+    //enviando uma requisicao HTTP POST para a API
+    this.http.post(environment.apiTarefas + '/tarefas', this.form.value)
+    .subscribe((response: any) => {//capturando a resposta da API
+      // exibindo uma mensagem de sucesso
+      this.mensagem = `Tarefa: ${response.titulo}, cadastrada com sucesso!`;
+      this.form.reset(); // limpando os campos do formulario
+    });
   }
 }
--- src/app/components/pages/consulta-tarefas/consulta-tarefas.component.ts
+++ src/app/components/pages/consulta-tarefas/consulta-tarefas.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-consulta-tarefas',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './consulta-tarefas.component.html',
   styleUrl: './consulta-tarefas.component.css'
 })
 export class ConsultaTarefasComponent {
 
+  //atributos
+  tarefas: any[] = []; //array de objetos
+
+  //injecoes de dependencia
+  http = inject(HttpClient);
+  fb = inject(FormBuilder);
+
+  //estrutura de formulario
+  form = this.fb.group({
+    dataMin: new FormControl('', [Validators.required]),
+    dataMax: new FormControl('', [Validators.required])
+  });
+
+  //funcao para capturar o submit do formulario
+  onSubmit() {
+    // guardando os falores do formulario em variaveis
+    const dataMin = this.form.value.dataMin;
+    const dataMax = this.form.value.dataMax;
+
+    //fazendo uma requisicao HTTP GET para a API
+    this.http.get(environment.apiTarefas + '/tarefas/' + dataMin + '/' + dataMax)
+      .subscribe((response) => { //aguardando a resposta da API
+        //armazenando os dados obtidos da API
+        this.tarefas = response as any[];
+      });
+  }
 }
